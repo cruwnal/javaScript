ce5eb4b90c6d064e24ce448a878031087bf9c3b8
Kunal Nagwanshi <nagwanshikunal23@gmail.com>
2022-08-10 23:46:21 +0530

    Add files via upload

---
+++ file26.js
@@ -0,0 +1,41 @@
+//intro to arrays 
+   //array is the reference type
+   //and all the refrence type is called object 
+// how to create arrays 
+
+//orderd collection of items- you can store anything in ordered number.
+
+
+
+
+let fruits = ["apple","mango","grapes"];
+
+console.log(fruits);//printing the array 
+console.log(typeof fruits);
+console.log(fruits[0]);
+
+fruits[1] = "banana";
+
+console.log(fruits);
+let number = [1,2,3,4];
+console.log(number);
+
+let mixed = [1,2,3,"string",null,undefined];
+console.log(mixed);
+ 
+//you can add anything in array 
+
+
+// it is giving me the single word like 
+//apple  mango  grapes
+//  0      1       2   
+
+// m a n g o
+// 0 1 2 3 4 
+
+
+console.log(fruits[1][4]);
+
+//answer - o
+
+// array indexing 
